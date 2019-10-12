568c6897b21688cf00cbc5f26ad03aa33e400a14
Yi-Cheng Lin <jack45j@gmail.com>
2019-10-12 16:53:58 +0800

    Initial Commit

---
+++ CoreMotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMotion
+//
+//  Created by Yi-Cheng Lin on 2019/10/12.
+//  Copyright © 2019 Yi-Cheng Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
